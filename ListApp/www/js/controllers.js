7b3a097994d47e294db0cde0ca89443a3341f1b9
DanielRomero <danielromero@mail.weber.edu>
2016-03-23 21:10:12 -0600

    front end stuff

--- ListApp/www/js/controllers.js
+++ ListApp/www/js/controllers.js
@@ -17,12 +17,34 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+/*.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
+})*/
+
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+  //  <!-- $scope.message = "Hello!";-->
+  $scope.chat = Chats.get($stateParams.chatId);
+
+  $scope.items = [{'name': 'My dream item', checked: false}];
+
+  $scope.addListItem = function (item) {
+    $scope.items.push({'name': item, checked: false});
+    //clear quote
+    this.newItem = null;
+    //newItem = ' ';
+
+  }
+  $scope.removeSelected = function () {
+    $scope.items = $scope.items.filter(function (item) {
+      return !item.checked;
+    })
+  }
 })
 
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
   };
+
+
 });
--- ListApp/www/js/services.js
+++ ListApp/www/js/services.js
@@ -4,32 +4,50 @@ angular.module('starter.services', [])
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
+  //
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    name: 'Groceries'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    name: 'Homework'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    name: 'Sex Positions I wanna try'
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    name: 'Grad Schools'
   }, {
     id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    name: 'Cheeses to eat before dead'
   }];
+
+/*var chats = [{
+  id: 0,
+  name: 'Ben Sparrow',
+  lastText: 'You on your way?',
+  face: 'img/ben.png'
+}, {
+  id: 1,
+  name: 'Max Lynx',
+  lastText: 'Hey, it\'s me',
+  face: 'img/max.png'
+}, {
+  id: 2,
+  name: 'Adam Bradleyson',
+  lastText: 'I should buy a boat',
+  face: 'img/adam.jpg'
+}, {
+  id: 3,
+  name: 'Perry Governor',
+  lastText: 'Look at my mukluks!',
+  face: 'img/perry.png'
+}, {
+  id: 4,
+  name: 'Mike Harrington',
+  lastText: 'This is wicked good ice cream.',
+  face: 'img/mike.png'
+}];*/
 
   return {
     all: function() {
